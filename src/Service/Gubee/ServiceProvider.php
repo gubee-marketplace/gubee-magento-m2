ac1f97de2407249a4f5a8deddfa820e8fc9cc77a
Matheus Heiden <matheus.heiden@bis2bis.com.br>
2026-07-07 14:54:23 -0300

    🐛 Fix Service Provider

--- src/Service/Gubee/ServiceProvider.php
+++ src/Service/Gubee/ServiceProvider.php
@@ -6,9 +6,18 @@ namespace Gubee\Integration\Service\Gubee;
 
 use Gubee\SDK\Api\ServiceProviderInterface;
 use Magento\Framework\ObjectManager\ObjectManager;
+use Magento\Framework\ObjectManager\FactoryInterface;
+use Magento\Framework\ObjectManager\ConfigInterface;
 
 class ServiceProvider extends ObjectManager implements ServiceProviderInterface
 {
+    public function __construct(FactoryInterface $factory, ConfigInterface $config, &$sharedInstances = [])
+    {
+        parent::__construct($factory, $config, $sharedInstances);
+        $factory->setObjectManager($this);
+        $this->_sharedInstances[ServiceProviderInterface::class] = $this;
+        $this->_sharedInstances[self::class] = $this;
+    }
     /**
      * Returns true if the container can return an entry for the given identifier.
      * Returns false otherwise.
