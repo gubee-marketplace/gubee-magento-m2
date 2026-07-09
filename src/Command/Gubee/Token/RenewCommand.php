08311153f56f1abfd8743b1dbe38a91eab396d6c
Matheus Heiden <matheus.heiden@bis2bis.com.br>
2026-07-09 09:31:54 -0300

    🐛 Fix authentication flow

--- src/Command/Gubee/Token/RenewCommand.php
+++ src/Command/Gubee/Token/RenewCommand.php
@@ -73,8 +73,6 @@ class RenewCommand extends AbstractCommand
                 "Token renewed successfully"
             );
         } catch (Exception $e) {
-            $this->config->setApiToken("");
-            $this->config->setApiTimeout(null);
             $this->getLogger()->error(
                 $e->getMessage()
             );
